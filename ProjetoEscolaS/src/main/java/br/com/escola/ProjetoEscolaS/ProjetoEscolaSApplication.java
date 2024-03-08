9f5f8c03eba2fcef484b70dae7f94a4dfc885d2d
SuziHarima <152184174+SuziHarima@users.noreply.github.com>
2024-03-08 15:17:08 -0300

    adiciona spring inicial

---
+++ ProjetoEscolaS/src/main/java/br/com/escola/ProjetoEscolaS/ProjetoEscolaSApplication.java
@@ -0,0 +1,13 @@
+package br.com.escola.ProjetoEscolaS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEscolaSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEscolaSApplication.class, args);
+	}
+
+}
---
+++ ProjetoEscolaS/src/test/java/br/com/escola/ProjetoEscolaS/ProjetoEscolaSApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.escola.ProjetoEscolaS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoEscolaSApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
