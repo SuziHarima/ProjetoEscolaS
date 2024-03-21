73c474a88dde8c66adcbdf129b1be482ffc8d1ba
SuziHarima <152184174+SuziHarima@users.noreply.github.com>
2024-03-21 10:44:20 -0300

    adiciona service: curso

--- ProjetoEscolaS/src/main/java/br/com/escola/ProjetoEscolaS/model/Curso.java
+++ ProjetoEscolaS/src/main/java/br/com/escola/ProjetoEscolaS/model/Curso.java
@@ -1,10 +1,15 @@
 package br.com.escola.ProjetoEscolaS.model;
 
-public class Cursos {
-    private int id;
+import lombok.*;
+
+import java.util.ArrayList;
+
+@Data
+public class Curso {
+    @Setter(AccessLevel.NONE) private Integer id;
     private String nome;
     private String descricao;
-    private int cargaHoraria;
+    private Integer cargaHoraria;
 
 
     private static int ultimoId = 0;
@@ -12,6 +17,15 @@ public class Cursos {
     private static int getNewId() {
         ultimoId++;
         return ultimoId;
+    }
+
+
+    @Getter static ArrayList<Curso> cursos = new ArrayList<>();
+
+    public static Curso salvar(Curso curso) {
+        curso.id = getNewId();
+        cursos.add(curso);
+        return curso;
     }
 
 
--- ProjetoEscolaS/src/main/java/br/com/escola/ProjetoEscolaS/service/CursoService.java
+++ ProjetoEscolaS/src/main/java/br/com/escola/ProjetoEscolaS/service/CursoService.java
@@ -1,2 +1,33 @@
-package br.com.escola.ProjetoEscolaS.service;public class CursoService {
+package br.com.escola.ProjetoEscolaS.service;
+
+import br.com.escola.ProjetoEscolaS.model.Curso;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CursoService {
+
+    public Curso salvar(Curso curso) throws Exception {
+        if (!validacao(curso)) {
+            return null;
+        }
+        return Curso.salvar(curso);
+
+    }
+
+    public boolean validacao(Curso curso) throws Exception {
+        if (curso.getNome() == null || curso.getNome().isBlank()) {
+            throw new Exception("Campo nome é obrigatório, favor preencher");
+        }
+        if (curso.getCargaHoraria() == null) {
+            throw new Exception("Campo carga horária é obrigatório, favor preencher");
+        }
+        return true;
+    }
+
+    public ArrayList<Curso> buscarTodos() throws Exception {
+        return Curso.getCursos();
+    }
+
 }
